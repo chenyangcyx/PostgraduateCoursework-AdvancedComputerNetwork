83a29b80576e6798f0f46b79d52cb8c8c4c3cc3c
chenyangcyx <chenyangcyx@outlook.com>
2020-12-21 23:32:14 +0800

    bug fix fixed the processing of the output of unused logger that was missed in LinuxDemo and RouterDemo
    commit time: 2020/12/21 23:32:08

--- chen.yang/Network_Telenet/Telnet/RouterDemo.py
+++ chen.yang/Network_Telenet/Telnet/RouterDemo.py
@@ -11,23 +11,23 @@ if __name__ == '__main__':
     ]
     logger = OutputLogger(True, True, "./telnet_log/%s.txt" % time.time())
     telnet_client_Router = TelnetClient(logger)
-    print("原始输出：")
+    logger.handleMsg("原始输出：")
     result_out = list()
     if telnet_client_Router.loginHostRouter(host_ip, password_login, password_enable):
         result_out = telnet_client_Router.executeSomeCommand(commands_router, "Router")
-    print("清理后的输出：")
+    logger.handleMsg("清理后的输出：")
     result_out2 = MessageHandle.handleAllMsg(result_out)
     for out in result_out2:
-        print(out)
+        logger.handleMsg(out)
 
-    # 测试其他功能
-    telnet_client = TelnetClient(logger)
-    # 使用telnetlib自带的interact()函数实现实时交互
-    # 不建议使用下面的函数！无法return值
-    # telnet_client.interactInCMD()
-    # 使用自己写的interact方法实现交互，Router
-    # 先执行一次，获取最初的前缀字符串
-    if telnet_client.loginHostRouter(host_ip, password_login, password_enable):
-        print(telnet_client.interactSendMsgRouter(""), end="")
-        while 1:
-            print(telnet_client.interactSendMsgRouter(input()), end="")
+    # # 测试其他功能
+    # telnet_client = TelnetClient(logger)
+    # # 使用telnetlib自带的interact()函数实现实时交互
+    # # 不建议使用下面的函数！无法return值
+    # # telnet_client.interactInCMD()
+    # # 使用自己写的interact方法实现交互，Router
+    # # 先执行一次，获取最初的前缀字符串
+    # if telnet_client.loginHostRouter(host_ip, password_login, password_enable):
+    #     logger.handleMsg(telnet_client.interactSendMsgRouter(""), end="")
+    #     while 1:
+    #         logger.handleMsg(telnet_client.interactSendMsgRouter(input()), end="")
