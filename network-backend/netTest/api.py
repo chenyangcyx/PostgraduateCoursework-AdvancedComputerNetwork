d30e9e00a67d5409d4115ed704647f08dc4c04eb
chenyangcyx <chenyangcyx@outlook.com>
2020-12-31 00:20:29 +0800

    Revert modify incorrect file path
    Since the python script is running with the path of the current script as the root directory, the original correct file path is incorrectly modified
    
    commit time: 2020/12/31 00:20:25

--- network-backend/netTest/api.py
+++ network-backend/netTest/api.py
@@ -5,9 +5,9 @@ from django.http import HttpResponse
 from netTest.Telnet import TelnetClient, OutputLogger, MessageHandle
 
 # 声明logger对象，用来屏幕输出和写文件记录
-logger = OutputLogger(True, True, "program_log/%s.txt" % time.strftime("%Y-%m-%d %H.%M.%S", time.localtime()))
+logger = OutputLogger(True, True, "netTest/program_log/%s.txt" % time.strftime("%Y-%m-%d %H.%M.%S", time.localtime()))
 # json文件的读取，全局唯一
-json_file = json.load(open("setting.json", 'r', encoding='utf-8'))
+json_file = json.load(open("netTest/setting.json", 'r', encoding='utf-8'))
 # telnet连接的映射，全局只创建一个对应路由器的连接
 telnet_dict = {}
 
